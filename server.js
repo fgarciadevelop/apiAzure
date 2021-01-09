ab8c3734f5a1c7fc6afb1cf1373efa72b0ba396e
Fran García <fgarciadevelop@gmail.com>
2021-01-09 20:42:49 +0100

    Cors enabled

--- server.js
+++ server.js
@@ -2,6 +2,10 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 80
 
+var cors = require('cors')
+
+app.use(cors())
+
 app.get('', (req, res) => {
     var today = new Date()
 
